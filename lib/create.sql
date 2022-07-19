bbc4900565f0c05df810cfd2fbbe9157a8f05588
Ben Botsford <ben.botsford@flatironschool.com>
2022-07-19 11:52:53 -0500

    remove solution

--- lib/create.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE bears (
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    age INTEGER,
-    sex CHAR(1),
-    color TEXT,
-    temperament TEXT,
-    alive BOOLEAN
-);
--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,41 +1,18 @@
-select_all_female_bears_return_name_and_age = """
-    SELECT
-        bears.name,
-        bears.age
-    FROM bears
-    WHERE sex='F';
+selects_all_female_bears_return_name_and_age = """
+    Write your SQL query here
 """
 
-select_all_bears_names_and_orders_in_alphabetical_order = """
-    SELECT
-        bears.name
-    FROM bears
-    ORDER BY bears.name ASC;
+selects_all_bears_names_and_orders_in_alphabetical_order = """
+    Write your SQL query here
 """
 
-select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    SELECT
-      bears.name,
-      bears.age
-    FROM bears
-    WHERE alive=1
-    ORDER BY bears.age ASC;
+selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
+    Write your SQL query here
 """
 
-select_oldest_bear_and_returns_name_and_age = """
-    SELECT
-      bears.name,
-      bears.age
-    FROM bears
-    ORDER BY bears.age DESC
-    LIMIT 1;
+selects_oldest_bear_and_returns_name_and_age = """
+    Write your SQL query here
 """
-
 select_youngest_bear_and_returns_name_and_age = """
-    SELECT
-      bears.name,
-      bears.age
-    FROM bears
-    ORDER BY bears.age ASC
-    LIMIT 1;
+    Write your SQL query here
 """
