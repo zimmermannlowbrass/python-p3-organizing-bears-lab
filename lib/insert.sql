34ecf81e8bac8f2e01d065eea49fc8f8bd4005f3
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-04-27 16:19:26 -0500

    completed labwork

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr.Chocolate', 1, 'M', 'Brown', 'fiesty', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 1, 'M', 'Brown', 'fiesty', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 1, 'M', 'Brown', 'fiesty', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 1, 'M', 'Brown', 'fiesty', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 1, 'M', 'Brown', 'fiesty', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 1, 'M', 'Brown', 'fiesty', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 1, 'M', 'Brown', 'fiesty', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, 'M', 'Brown', 'fiesty', 0);
--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,18 +1,41 @@
 select_all_female_bears_return_name_and_age = """
-    Write your SQL query here
+    SELECT
+        bears.name,
+        bears.age
+    FROM bears
+    WHERE sex='F';
 """
 
 select_all_bears_names_and_orders_in_alphabetical_order = """
-    Write your SQL query here
+    SELECT
+        bears.name
+    FROM bears
+    WHERE bears.name IS NOT 'None'
+    ORDER BY bears.name ASC;
 """
 
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    Write your SQL query here
+    SELECT
+        bears.name,
+        bears.age
+    FROM bears
+    WHERE bears.alive IS 1
+    ORDER BY bears.age ASC;
 """
 
 select_oldest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+    SELECT
+        bears.name,
+        bears.age
+    FROM bears
+    ORDER BY bears.age DESC
+    LIMIT 1;
 """
 select_youngest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+    SELECT
+        bears.name,
+        bears.age
+    FROM bears
+    ORDER BY bears.age ASC
+    LIMIT 1;
 """
